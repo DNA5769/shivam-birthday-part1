63872b1aef0a4dc7264dafac26f1168c9baa6cf9
DNA5769 <dennisthomas2002@gmail.com>
2020-10-16 14:23:15 +0300

    Started on wishes

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+let wishes = require(path.join(__dirname, 'src', 'w.json'));
+
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+
+app.get('/wishes', (req, res) => {
+  res.render('w.ejs', {'wishes': wishes});
 });
 
 const PORT = process.env.PORT || 3000;
