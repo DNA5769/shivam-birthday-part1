a665c336d2cd7442a8f38010eb70400e1ae2867b
DNA5769 <dennisthomas2002@gmail.com>
2020-10-16 13:42:32 +0300

    Small changes and added ejs

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('public'));
-const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`[STARTING]: Server started on port ${PORT}`);
 });
